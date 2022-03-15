185b03bc8387f6374f8ad6a6d2fde93d35630ba5
Patrick <patrick.mumot@gmail.com>
2022-03-14 20:56:20 -0400

    Coffee Machine OOP commit

--- CoffeeMachineOOP/main.py
+++ CoffeeMachineOOP/main.py
@@ -3,19 +3,19 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 isOn = True
+coffee_menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
 
 while isOn:
-    choice = input("What would you like? (" + Menu.get_items() + "):")
+    choice = input(f"What would you like? ({coffee_menu.get_items()}):")
     if choice == "off":
         exit()
     elif choice == "report":
-        CoffeeMaker.report()
-        MoneyMachine.report()
+        coffee_maker.report()
+        money_machine.report()
     else:
-        coffee = Menu.find_drink(choice)
-        if CoffeeMaker.is_resource_sufficient(coffee):
-            money_paid = MoneyMachine.process_coins()
-            if MoneyMachine.make_payment(money_paid):
-                CoffeeMaker.make_coffee(coffee)
-
-
+        coffee = coffee_menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(coffee) and money_machine.make_payment(coffee.cost):
+            coffee_maker.make_coffee(coffee)
